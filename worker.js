706686b5157ff536c8f42c7428fef96462bb40fc
AiratTop <airat.halitov@gmail.com>
2026-01-26 01:05:48 +0300

    Add noindex, nofollow

--- worker.js
+++ worker.js
@@ -30,7 +30,8 @@ const WORD_MATRIX = (() => {
 const CORS_HEADERS = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type'
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'X-Robots-Tag': 'noindex, nofollow'
 };
 
 // Pick the correct Russian word ending based on the number.
